767fc3a9a56eb985e42c3a1fb67cdeeeb7d141f1
James Mullen <james.mullen@bjss.com>
2019-06-13 09:02:10 +0100

    Up the version

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World - v5!";
+        return "Hello World - v6!";
     }
 
     public static void main(String[] args) throws Exception {
